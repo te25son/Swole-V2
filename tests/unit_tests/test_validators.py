af69825938c7bbffc0c69f6a317183c7a3c3162b
Timothy Eason <39095798+te25son@users.noreply.github.com>
2023-03-30 15:52:02 +0200

    Feat: Added tests to increase coverage to 100 (#53)
    Co-authored-by: Timothy Eason <timothy.eason@rossum.ai>

--- tests/unit_tests/test_validators.py
+++ tests/unit_tests/test_validators.py
@@ -5,8 +5,13 @@ from hypothesis import given
 from hypothesis import strategies as st
 
 from swole_v2.errors.exceptions import BusinessError
-from swole_v2.errors.messages import INCORRECT_DATE_FORMAT, INVALID_ID
-from swole_v2.schemas.validators import check_date_format, check_is_uuid
+from swole_v2.errors.messages import (
+    FIELD_CANNOT_BE_EMPTY,
+    INCORRECT_DATE_FORMAT,
+    INVALID_ID,
+    MUST_BE_A_VALID_NON_NEGATIVE_NUMBER,
+)
+from swole_v2.schemas.validators import check_date_format, check_empty_string, check_is_non_negative, check_is_uuid
 
 
 @given(value=st.uuids())
@@ -26,3 +31,15 @@ def test_check_date_format(value: str) -> None:
     with pytest.raises(BusinessError) as ex:
         check_date_format(allow_none=False)(value)  # type: ignore
     assert str(ex.value) == INCORRECT_DATE_FORMAT
+
+
+def test_check_empty_string_raises_error_when_none() -> None:
+    with pytest.raises(BusinessError) as ex:
+        check_empty_string(field_name="name", allow_none=False)(None)  # type: ignore
+    assert str(ex.value) == FIELD_CANNOT_BE_EMPTY.format("name")
+
+
+def test_non_negative_fails() -> None:
+    with pytest.raises(BusinessError) as ex:
+        check_is_non_negative(allow_none=True)("not a negative number")  # type: ignore
+    assert str(ex.value) == MUST_BE_A_VALID_NON_NEGATIVE_NUMBER
