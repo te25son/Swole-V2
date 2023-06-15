ec3c0e178c10e21fa654e59bc81424be9a5ccdb7
Timothy Eason <te25son@gmail.com>
2023-06-15 08:13:03 +0200

    use relative import on models

--- src/swole_v2/models/__init__.py
+++ src/swole_v2/models/__init__.py
@@ -1,8 +1,8 @@
-from swole_v2.models.exercise import Exercise, ExerciseProgressReport, ExerciseProgressReportData, ExerciseRead
-from swole_v2.models.set import Set, SetRead
-from swole_v2.models.token import Token
-from swole_v2.models.user import User, UserRead
-from swole_v2.models.workout import Workout, WorkoutRead
+from .exercise import Exercise, ExerciseProgressReport, ExerciseProgressReportData, ExerciseRead
+from .set import Set, SetRead
+from .token import Token
+from .user import User, UserRead
+from .workout import Workout, WorkoutRead
 
 Set.update_forward_refs(Workout=Workout, Exercise=Exercise)
 Workout.update_forward_refs(Exercise=Exercise)
